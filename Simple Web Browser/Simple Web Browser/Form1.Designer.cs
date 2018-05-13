d84d976ef1913489072e7e1f63f72956c5d7ca0d
Lochlan Kennedy <38372089+Pharah-tato@users.noreply.github.com>
2018-05-13 13:11:18 +0800

    Web Browser Program Added
    added First Program

---
+++ Simple Web Browser/Simple Web Browser/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace Simple_Web_Browser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.backBtn = new System.Windows.Forms.Button();
+            this.forwardBtn = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.searchBtn = new System.Windows.Forms.Button();
+            this.homeBtn = new System.Windows.Forms.Button();
+            this.refreshBtn = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.setHomeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // backBtn
+            // 
+            this.backBtn.Location = new System.Drawing.Point(5, 35);
+            this.backBtn.Name = "backBtn";
+            this.backBtn.Size = new System.Drawing.Size(43, 23);
+            this.backBtn.TabIndex = 0;
+            this.backBtn.Text = "<";
+            this.backBtn.UseVisualStyleBackColor = true;
+            this.backBtn.Click += new System.EventHandler(this.backBtn_Click);
+            // 
+            // forwardBtn
+            // 
+            this.forwardBtn.Location = new System.Drawing.Point(54, 35);
+            this.forwardBtn.Name = "forwardBtn";
+            this.forwardBtn.Size = new System.Drawing.Size(43, 23);
+            this.forwardBtn.TabIndex = 1;
+            this.forwardBtn.Text = ">";
+            this.forwardBtn.UseVisualStyleBackColor = true;
+            this.forwardBtn.Click += new System.EventHandler(this.forwardBtn_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(103, 37);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(693, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // searchBtn
+            // 
+            this.searchBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.searchBtn.Location = new System.Drawing.Point(802, 35);
+            this.searchBtn.Name = "searchBtn";
+            this.searchBtn.Size = new System.Drawing.Size(75, 23);
+            this.searchBtn.TabIndex = 3;
+            this.searchBtn.Text = "Search";
+            this.searchBtn.UseVisualStyleBackColor = true;
+            this.searchBtn.Click += new System.EventHandler(this.searchBtn_Click);
+            // 
+            // homeBtn
+            // 
+            this.homeBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.homeBtn.Location = new System.Drawing.Point(883, 35);
+            this.homeBtn.Name = "homeBtn";
+            this.homeBtn.Size = new System.Drawing.Size(75, 23);
+            this.homeBtn.TabIndex = 4;
+            this.homeBtn.Text = "Home";
+            this.homeBtn.UseVisualStyleBackColor = true;
+            this.homeBtn.Click += new System.EventHandler(this.homeBtn_Click);
+            // 
+            // refreshBtn
+            // 
+            this.refreshBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.refreshBtn.Location = new System.Drawing.Point(964, 35);
+            this.refreshBtn.Name = "refreshBtn";
+            this.refreshBtn.Size = new System.Drawing.Size(75, 23);
+            this.refreshBtn.TabIndex = 5;
+            this.refreshBtn.Text = "Reload";
+            this.refreshBtn.UseVisualStyleBackColor = true;
+            this.refreshBtn.Click += new System.EventHandler(this.refreshBtn_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1055, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.setHomeToolStripMenuItem});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser1.Location = new System.Drawing.Point(0, 65);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(1055, 527);
+            this.webBrowser1.TabIndex = 7;
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // setHomeToolStripMenuItem
+            // 
+            this.setHomeToolStripMenuItem.Name = "setHomeToolStripMenuItem";
+            this.setHomeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.setHomeToolStripMenuItem.Text = "Set Home";
+            this.setHomeToolStripMenuItem.ToolTipText = "Sets the Home page to current Page";
+            this.setHomeToolStripMenuItem.Click += new System.EventHandler(this.setHomeToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1055, 592);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.refreshBtn);
+            this.Controls.Add(this.homeBtn);
+            this.Controls.Add(this.searchBtn);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.forwardBtn);
+            this.Controls.Add(this.backBtn);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Simple Web Browser";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button backBtn;
+        private System.Windows.Forms.Button forwardBtn;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button searchBtn;
+        private System.Windows.Forms.Button homeBtn;
+        private System.Windows.Forms.Button refreshBtn;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem setHomeToolStripMenuItem;
+    }
+}
+
---
+++ Simple Web Browser/Simple Web Browser/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Simple_Web_Browser
+{
+    
+    public partial class Form1 : Form
+    {
+        private String homeUrl = "https://google.com";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //if can go back then go back.
+        private void backBtn_Click(object sender, EventArgs e)
+        {
+            if (webBrowser1.CanGoBack)
+            {
+                webBrowser1.GoBack();
+
+            }
+        }
+
+        //checks if can go forward if true then goes forward
+        private void forwardBtn_Click(object sender, EventArgs e)
+        {
+            if (webBrowser1.CanGoForward)
+            {
+                webBrowser1.GoForward();
+            }
+        }
+
+        //goes to the home url
+        private void homeBtn_Click(object sender, EventArgs e)
+        {
+            NavigateBrowser(homeUrl);
+        }
+
+        //refreshes the browser
+        private void refreshBtn_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Refresh();
+        }
+
+        //searches
+        private void searchBtn_Click(object sender, EventArgs e)
+        {
+            searchButton();
+        }
+
+        //ends the program
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        //sets the home page to the current url of the web browser
+        private void setHomeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            homeUrl = webBrowser1.Url.ToString();
+        }
+
+        //tool to stop writing webBrowser1.Navigate it is also just as dynamic as a string needs to be passed.
+        private void NavigateBrowser(String webUrl)
+        {
+
+            webBrowser1.Navigate(webUrl);
+        }
+
+        //shows about information
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            form2 Form2 = new form2();
+            Form2.ShowDialog();
+        }
+
+        //sets home on load
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            NavigateBrowser(homeUrl);
+        }
+
+        //creates a tool to reduce copy pasting of same search code when pressing buttons or by pressing enter.
+        private void searchButton()
+        {
+
+            if (textBox1.Text != "")
+            {
+                if (!textBox1.Text.Contains("http://") || !textBox1.Text.Contains("http://"))
+                {
+                    textBox1.Text = "http://" + textBox1.Text;
+                    NavigateBrowser(textBox1.Text);
+                    if (webBrowser1.ReadyState == WebBrowserReadyState.Complete)
+                    {
+                        textBox1.Text = webBrowser1.Url.ToString();
+                    }
+                }
+                else
+                {
+                    
+                    NavigateBrowser(textBox1.Text);
+                    if (webBrowser1.ReadyState == WebBrowserReadyState.Complete)
+                    {
+                        textBox1.Text = webBrowser1.Url.ToString();
+                    }
+                    
+
+                }
+            }
+        }
+        //when enter is pressed run searchButton()
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                searchBtn.Focus();
+                searchButton();
+                e.SuppressKeyPress = true;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Back)
+            {
+                if (webBrowser1.CanGoBack) webBrowser1.GoBack();
+            }
+        }
+    }
+}
